a4de7aa3d5f9e7bf4259d99d92b130464a374c3b
Guilherme Lima <guilhermefk123@gmail.com>
2025-05-18 23:33:01 -0300

    Ajustes no projeto
    Adicionei biblioteca partículas.js e fiz umas correções no geral do código!

--- Código/script.js
+++ Código/script.js
@@ -177,4 +177,19 @@ const observerGitGithub = new IntersectionObserver((entries) => {
         }
     });
 });
+
 observerGitGithub.observe(GitGithub);
+
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS('particles-js', {
+        particles: {
+            number: { value: 80, density: { enable: true, value_area: 800 } },
+            color: { value: "#ffffff" },
+            shape: { type: "circle" },
+            opacity: { value: 0.5, random: true },
+            size: { value: 3, random: true },
+            line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
+            move: { enable: true, speed: 2, direction: "none", random: true, straight: false, out_mode: "out" }
+        }
+    });
+});
